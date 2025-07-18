b94fbc12c3be8ad61f1d9ef69214a74515c08f09
Luis <luis.e.ruiz1209@gmail.com>
2025-07-18 11:00:22 -0500

    Changing type of variables

--- app/src/main/java/com/example/baseballbuddy/ui/theme/models/Player.kt
+++ app/src/main/java/com/example/baseballbuddy/ui/theme/models/Player.kt
@@ -4,53 +4,63 @@ import com.google.gson.annotations.SerializedName
 
 data class Player(
     @SerializedName("id")
-    val id: Int? = null,
+    val id: Int,
 
     @SerializedName("first_name")
-    val firstName: String? = null,
+    val firstName: String,
 
     @SerializedName("last_name")
-    val lastName: String? = null,
+    val lastName: String,
 
     @SerializedName("full_name")
-    val fullName: String? = null,
+    val fullName: String,
 
     @SerializedName("debut_year")
-    val debutYear: Int? = null,
+    val debutYear: Int,
 
     @SerializedName("jersey")
-    val jersey: String? = null,
+    val jersey: String,
 
+    /**
+     * Nullable because not all players go to college.
+     */
     @SerializedName("college")
     val college: String? = null,
 
     @SerializedName("position")
-    val position: String? = null,
+    val position: String,
 
     @SerializedName("active")
-    val active: Boolean? = null,
+    val active: Boolean,
 
     @SerializedName("birth_place")
-    val birthPlace: String? = null,
+    val birthPlace: String,
 
     @SerializedName("dob")
-    val dob: String? = null,
+    val dob: String,
 
     @SerializedName("age")
-    val age: Int? = null,
+    val age: Int,
 
     @SerializedName("height")
-    val height: String? = null,
+    val height: String,
 
     @SerializedName("weight")
-    val weight: String? = null,
+    val weight: String,
 
+    /**
+     * Nullable because international players are not drafted.
+     * So this value may be null for international players.
+     */
     @SerializedName("draft")
     val draft: String? = null,
 
     @SerializedName("bats_throws")
-    val batsThrows: String? = null,
+    val batsThrows: String,
 
+    /**
+     * Nullable because if a player is a free agent, this may come as null.
+     */
     @SerializedName("team")
     val team: Team? = null
 )
