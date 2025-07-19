516de60cfb73aeda686b8d6057ff1357ce03fc63
Luis <luis.e.ruiz1209@gmail.com>
2025-07-18 22:42:32 -0500

    New request class

---
+++ app/src/main/java/com/example/baseballbuddy/ui/theme/network/BallDontLieTeamsRequest.kt
@@ -0,0 +1,6 @@
+package com.example.baseballbuddy.ui.theme.network
+
+import com.example.baseballbuddy.ui.theme.models.Division
+import com.example.baseballbuddy.ui.theme.models.League
+
+data class BallDontLieTeamsRequest(val league: League? = null, val division: Division? = null)
