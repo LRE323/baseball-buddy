80a91db8ccb75802a2b87d1e8d18fd9fedc4112e
Luis <luis.e.ruiz1209@gmail.com>
2025-07-19 11:56:06 -0500

    New Activity

---
+++ app/src/main/java/com/example/baseballbuddy/ui/theme/activities/TeamInfoActivity.kt
@@ -0,0 +1,10 @@
+package com.example.baseballbuddy.ui.theme.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class TeamInfoActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
