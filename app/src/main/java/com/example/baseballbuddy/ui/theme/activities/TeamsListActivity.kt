5448a4f9a8cb782d0e4a96d86a1af6d56ef1bd06
Luis <luis.e.ruiz1209@gmail.com>
2025-07-19 11:39:51 -0500

    Imports

--- app/src/main/java/com/example/baseballbuddy/ui/theme/activities/TeamsListActivity.kt
+++ app/src/main/java/com/example/baseballbuddy/ui/theme/activities/TeamsListActivity.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.baseballbuddy.R
 import com.example.baseballbuddy.ui.theme.BaseballBuddyTheme
