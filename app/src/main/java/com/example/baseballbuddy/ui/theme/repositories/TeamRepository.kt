f3300b3da4db942e5ec58b7c33f5e574e361e657
Luis <luis.e.ruiz1209@gmail.com>
2025-07-18 22:43:24 -0500

    Adding query parameters to API call

--- app/src/main/java/com/example/baseballbuddy/ui/theme/repositories/TeamRepository.kt
+++ app/src/main/java/com/example/baseballbuddy/ui/theme/repositories/TeamRepository.kt
@@ -2,11 +2,15 @@ package com.example.baseballbuddy.ui.theme.repositories
 
 import com.example.baseballbuddy.ui.theme.models.TeamListResponse
 import com.example.baseballbuddy.ui.theme.network.BallDontLieNetworkModule
+import com.example.baseballbuddy.ui.theme.network.BallDontLieTeamsRequest
 
 class TeamRepository {
     private val teamApiService = BallDontLieNetworkModule.ballDontLieApiService
 
-    suspend fun getTeamListResponse(): TeamListResponse {
-        return teamApiService.getTeams()
+    suspend fun getTeamListResponse(request: BallDontLieTeamsRequest? = null): TeamListResponse {
+        return teamApiService.getTeams(
+            league = request?.league?.serviceQueryLabel,
+            division = request?.division?.serviceQueryLabel
+        )
     }
 }
