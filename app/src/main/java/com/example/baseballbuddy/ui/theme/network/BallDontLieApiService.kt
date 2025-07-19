b7bd625c77dfed3500e3fdeea8f5fd8e109261a2
Luis <luis.e.ruiz1209@gmail.com>
2025-07-18 22:43:06 -0500

    Adding query parameters to API call

--- app/src/main/java/com/example/baseballbuddy/ui/theme/network/BallDontLieApiService.kt
+++ app/src/main/java/com/example/baseballbuddy/ui/theme/network/BallDontLieApiService.kt
@@ -2,11 +2,15 @@ package com.example.baseballbuddy.ui.theme.network
 
 import com.example.baseballbuddy.ui.theme.models.TeamListResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface BallDontLieApiService {
 
     @GET("mlb/v1/teams")
-    suspend fun getTeams(): TeamListResponse
+    suspend fun getTeams(
+        @Query("league") league: String? = null,
+        @Query("division") division: String? = null
+    ): TeamListResponse
 
     @GET("mlb/v1/players")
     suspend fun getPlayers(): TeamListResponse
